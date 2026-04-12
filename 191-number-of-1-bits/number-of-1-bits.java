7c55a8a222629686d4a7f24c9257d65c325c0cba
Beriyev <itsamevirus@gmail.com>
2026-04-12 11:20:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (93.00%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,14 +1,10 @@
 class Solution {
     public int hammingWeight(int n) {
-        String binary = Integer.toBinaryString(n);
         int count = 0;
-        int i;
-        for(i=0;i<binary.length();i++)
+        while(n!=0)
         {
-            if(binary.charAt(i)=='1')
-            {
-                count++;
-            }
+            count = count + (n&1);
+            n = n>>1;
         }
         return count;
     }
