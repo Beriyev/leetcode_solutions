ddf30ec7e4197a77a41688697e6785bf287b3e8a
Beriyev <itsamevirus@gmail.com>
2026-05-22 10:10:58 +0530

    Time: 18 ms (14.17%) | Memory: 52 MB (83.89%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -6,14 +6,11 @@ class Solution {
         int i;
         for(i=0;i<nums.length;i++)
         {
-            if(!hash.containsKey(nums[i])||hash.get(nums[i])!=-1)
-            {
-                hash.put(nums[i],hash.getOrDefault(nums[i],0)+1);
-            }
-            if(hash.get(nums[i])>flr)
+            hash.put(nums[i],hash.getOrDefault(nums[i],0)+1);
+
+            if(hash.get(nums[i])==flr+1)
             {
                 ans.add(nums[i]);
-                hash.put(nums[i],-1);
             }
         }
         return ans;
