4249b786b9e6a7a1daeccd22a6d9d5e93a40ffc9
Beriyev <itsamevirus@gmail.com>
2026-05-23 10:50:13 +0530

    Time: 11 ms (34.60%) | Memory: 58.6 MB (99.37%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,23 +10,72 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ArrayList<Integer> arraylist = new ArrayList<>();
-        ListNode temp = head;
-        while(temp!=null)
+
+        if(head==null||head.next==null)
         {
-            arraylist.add(temp.val);
-            temp = temp.next;
+            return head;
         }
-        Integer[] arr = arraylist.toArray(new Integer[0]);
-        Arrays.sort(arr);
-        temp = head;
-        int i = 0;
-        while(temp!=null)
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(fast!=null&&fast.next!=null)
         {
-            temp.val = arr[i];
-            i++;
-            temp = temp.next;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        return head;
+
+        ListNode second = slow.next;
+        slow.next = null;
+        ListNode first = head;
+        
+        ListNode left = sortList(first);
+        ListNode right = sortList(second);
+
+        return merge(left,right);
+    }
+
+    public ListNode merge(ListNode left, ListNode right)
+    {
+        ListNode temphead = new ListNode(0);
+        ListNode temp = temphead;
+
+        while(left!=null&&right!=null)
+        {
+            if(left.val<=right.val)
+            {
+                temp.next = left;
+                left = left.next;
+                temp = temp.next;
+            }
+            else if(right.val<=left.val)
+            {
+                temp.next = right;
+                right = right.next;
+                temp = temp.next;
+            }
+        }
+
+        if(left==null)
+        {
+            while(right!=null)
+            {
+                temp.next = right;
+                right = right.next;
+                temp = temp.next;
+            }
+        }
+
+        if(right==null)
+        {
+            while(left!=null)
+            {
+                temp.next = left;
+                left = left.next;
+                temp = temp.next;
+            }
+        }
+
+        return temphead.next;
     }
 }
