a408cc778204fa2553459f1cd30e8266596084d3
Beriyev <itsamevirus@gmail.com>
2026-06-25 18:31:49 +0530

    Time: 1 ms (86.56%) | Memory: 42.6 MB (90.20%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        int i,j;
+        for(i=0;i<nums.length-1;i++)
+        {
+            for(j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
