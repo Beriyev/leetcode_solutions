159cda71a0390c9b321333a6555deadc616b8e67
Beriyev <itsamevirus@gmail.com>
2026-06-27 10:31:12 +0530

    Time: 1 ms (100.00%) | Memory: 62.1 MB (85.81%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        int i;
+
+        for(i=0;i<nums.length;i++)
+        {
+            min = Math.min(min,nums[i]);
+            max = Math.max(max,nums[i]);
+        }
+
+        return (long) k*(max-min);
+    }
+}
