4c6f15fb5a1b90a88e643d8555cefaa2c724b4ae
Beriyev <itsamevirus@gmail.com>
2026-07-05 12:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (7.37%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -14,14 +14,12 @@
  * }
  */
 class Solution {
+    int depth = 0;
     public int maxDepth(TreeNode root) {
         if(root==null)
         {
             return 0;
         }
-        else
-        {
-            return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
-        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
     }
 }
